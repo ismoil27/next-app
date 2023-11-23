27e4afcc35466d7104a149b7bca05ab9e588603e
ismoil27 <abduraimovismoil98@gmail.com>
2023-11-23 15:01:35 +0900

    feat: developing tailwind styles and Image

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from "./dojo-logo.png";
 
 const Navbar = () => {
   return (
     <nav>
-      <h1>Justin Project</h1>
+      <Image
+        src={Logo}
+        alt="Logo for heldesk"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Justin Helpdesk</h1>
       <Link href={"/"}>Dashboard</Link>
       <Link href={"/tickets"}>Tickets</Link>
     </nav>
